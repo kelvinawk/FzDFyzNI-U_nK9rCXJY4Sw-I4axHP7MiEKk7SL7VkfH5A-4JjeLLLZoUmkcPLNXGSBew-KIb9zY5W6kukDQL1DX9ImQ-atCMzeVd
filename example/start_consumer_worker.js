2f4cb84011c088964898d57d552353fdec2d21ba
Kelvin Kwan <kelvinkwan1991@gmail.com>
2016-02-05 12:01:13 +0800

    fix syntax error of start_producer_worker

--- example/start_consumer_worker.js
+++ example/start_consumer_worker.js
@@ -1,8 +1,8 @@
 'use strict';
 
 //import declaration
-var beanstalkd_config = require("./lib/config/beanstalkd_config");
-var ConsumerWorker = require("./lib/consumer_worker");
+var beanstalkd_config = require("../lib/config/beanstalkd_config");
+var ConsumerWorker = require("../lib/consumer_worker");
 
 //create new consumer worker
 let consumer_worker = new ConsumerWorker(beanstalkd_config);
