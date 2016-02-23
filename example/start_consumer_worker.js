c5b57d0528d5c3716519e698dd37823dc21c2698
Kelvin Kwan <kelvinkwan1991@gmail.com>
2016-02-23 19:35:47 +0800

    modify all files with eslint checking

--- example/start_consumer_worker.js
+++ example/start_consumer_worker.js
@@ -1,10 +1,10 @@
 'use strict';
 
-//import declaration
-var beanstalkd_config = require("../lib/config/beanstalkd_config");
-var ConsumerWorker = require("../lib/consumer_worker");
+// import declaration
+let beanstalkd_config = require('../lib/config/beanstalkd_config');
+let ConsumerWorker = require('../lib/consumer_worker');
 
-//create new consumer worker
+// create new consumer worker
 let consumer_worker = new ConsumerWorker(beanstalkd_config);
-//start worker
+// start worker
 consumer_worker.start();
--- example/start_producer_worker.js
+++ example/start_producer_worker.js
@@ -1,16 +1,16 @@
 'use strict';
 
-//import declaration
-var beanstalkd_config = require("../lib/config/beanstalkd_config");
-var ProducerWorker = require("../lib/producer_worker");
-var Seed = require("../lib/seed");
+// import declaration
+let beanstalkd_config = require('../lib/config/beanstalkd_config');
+let ProducerWorker = require('../lib/producer_worker');
+let Seed = require('../lib/seed');
 
-//create seed and initialize producer worker
-let seed = new Seed("USD", "HKD", 0, 0, 0, 60);
+// create seed and initialize producer worker
+let seed = new Seed('USD', 'HKD', 0, 0, 0, 60);
 let producer_worker = new ProducerWorker(beanstalkd_config);
 
-//put seed into tube
-producer_worker.put(seed, 0).then(function(jobid){
+// put seed into tube
+producer_worker.put(seed, 0).then(function (jobid) {
 	console.log(`[${producer_worker.constructor.name}] job is put into tube: ${beanstalkd_config.tube_name}, jobid: ${jobid}`);
 	console.log(`[${producer_worker.constructor.name}] seed in job: ${JSON.stringify(seed)}`);
 });
