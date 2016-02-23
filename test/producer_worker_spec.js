5010a8c9e8212b02ec82b0d500bf39b4d22a580e
Kelvin Kwan <kelvinkwan1991@gmail.com>
2016-02-23 20:37:53 +0800

    modify testing file with eslint checking

--- test/producer_worker_spec.js
+++ test/producer_worker_spec.js
@@ -1,24 +1,22 @@
 'use strict';
 
 // import declaration
-var chai = require("chai");
-var expect = require("chai").expect;
-var ProducerWorker = require("../lib/producer_worker.js");
-var beanstalkd_config = require("../lib/config/beanstalkd_config.js");
-var Seed = require("../lib/seed");
+let expect = require('chai').expect;
+let ProducerWorker = require('../lib/producer_worker.js');
+let beanstalkd_config = require('../lib/config/beanstalkd_config.js');
+let Seed = require('../lib/seed');
 
-describe("ProducerWorker", function(){
-	describe("#put(seed, delay)", function(){
-		it("return jobid if the seed is put into tube", function(){
-
-			//create seed and initialize producer worker
-			let seed = new Seed("USD", "HKD", 0, 0, 0, 60);
+describe('ProducerWorker', function () {
+	describe('#put(seed, delay)', function () {
+		it('return jobid if the seed is put into tube', function () {
+			// create seed and initialize producer worker
+			let seed = new Seed('USD', 'HKD', 0, 0, 0, 60);
 			let producer_worker = new ProducerWorker(beanstalkd_config);
-			
-			//put seed into tube
-			producer_worker.put(seed,0).then(function(jobid){
+
+			// put seed into tube
+			producer_worker.put(seed, 0).then(function (jobid) {
 				expect(jobid).to.be.a('Number');
 			});
-		})
-	})
+		});
+	});
 });
